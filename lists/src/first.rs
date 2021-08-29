3e5b89d07893190b772fc4e481f4bbc304c6f725
Takaya Omura <takaya.omura@outlook.com>
2021-08-29 16:29:29 +0900

    first

---
+++ lists/src/first.rs
@@ -0,0 +1,50 @@
+use std::mem;
+
+pub struct List {
+  head: Link,
+}
+
+enum Link {
+  Empty, 
+  More(Box<Node>),
+}
+
+struct Node {
+  elem: i32,
+  next: Link,
+}
+
+impl List {
+  pub fn new() -> Self {
+    Self{ head: Link::Empty }
+  }
+
+  pub fn push(&mut self, elem: i32) {
+    let new_node = Box::new(Node {
+      elem: elem,
+      next: mem::replace(&mut self.head, Link::Empty),
+    });
+    self.head = Link::More(new_node)
+  }
+
+  pub fn pop(&mut self) -> Option<i32> {
+    match mem::replace(&mut self.head, Link::Empty) {
+      Link::Empty => None,
+      Link::More(node) => {
+        self.head = node.next;
+        Some(node.elem)
+      }
+    }
+  }
+}
+
+
+#[cfg(test)]
+mod test {
+  use super::List;
+  #[test]
+  fn basics() {
+    let mut list = List::new();
+    assert_eq!(list.pop(), None);
+  }
+}
---
+++ lists/src/main.rs
@@ -0,0 +1,6 @@
+use lists::first::List;
+
+fn main() {
+  // let list: List = List::Pair(1, Box::new(List::Pair(2, Box::new(List::Nil))));
+  // println!("{:?}", list);
+}
